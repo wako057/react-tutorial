19330b77cb7bbf305eacc5f4ca5f5503dd1ed835
Gregory Gerard <gregory@flatchr.io>
2023-11-04 19:14:12 +0100

    feat: course 50

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
+import TabButton2 from "./components/TabButton/TabButton2";
 
 function App() {
+    function handleSelect() {
+        console.log('Hello World - Selected');
+    }
+
     return (
         <div>
             <header><h1>Hello World!</h1></header>
@@ -20,6 +26,16 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[2]} />
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
+                </section>
+                <section id="examples">
+                    <h2>Example</h2>
+                    <menu>
+                            <TabButton onSelect={handleSelect}>Component</TabButton>
+                            <TabButton onSelect={handleSelect}>JSX</TabButton>
+                            <TabButton onSelect={handleSelect}>Props</TabButton>
+                            <TabButton onSelect={handleSelect}>States</TabButton>
+                        {/*<TabButton2 label='Component 2'></TabButton2>*/}
+                    </menu>
                 </section>
                 <h2>Time to get started!</h2>
             </main>
---
+++ src/components/TabButton/TabButton2.jsx
@@ -0,0 +1,3 @@
+export default function TabButton2({label}) {
+    return <li><button>{label}</button></li>
+}
