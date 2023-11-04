6a33e4e658aa35c657af755b76a32887ccc3c6fd
Gregory Gerard <gregory@flatchr.io>
2023-11-04 23:46:15 +0100

    feat: course 55 - 1 - conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {CORE_CONCEPTS} from "./data";
+import {CORE_CONCEPTS, EXAMPLES} from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
@@ -6,7 +6,7 @@ import TabButton2 from "./components/TabButton/TabButton2";
 import { useState } from 'react';
 
 function App() {
-    const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+    const [ selectedTopic, setSelectedTopic ] = useState();
 
     function handleSelect(selectedButton) {
         setSelectedTopic(selectedButton)
@@ -35,13 +35,23 @@ function App() {
                 <section id="examples">
                     <h2>Example</h2>
                     <menu>
-                            <TabButton onSelect={() => handleSelect('component')}>Component</TabButton>
+                            <TabButton onSelect={() => handleSelect('components')}>Component</TabButton>
                             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
                             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                             <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
                         {/*<TabButton2 label='Component 2'></TabButton2>*/}
                     </menu>
-                    {selectedTopic}
+                        { !selectedTopic ? (
+                         <p>Please select a topic.</p>
+                        ) : (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )}
                 </section>
                 <h2>Time to get started!</h2>
             </main>
